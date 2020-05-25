20716c779500046aeb3e578cd748f0896b1b9efb
Asad Ali <alidjasad@gmail.com>
2020-05-25 23:11:09 +0530

    lodash 4.17.15

---
+++ mod.ts
@@ -0,0 +1,8 @@
+import { createRequire } from "https://deno.land/std/node/module.ts";
+const require = createRequire(import.meta.url);
+// Loads native module polyfill.
+const path = require("path");
+
+const _ = require('lodash');
+
+export default _;
