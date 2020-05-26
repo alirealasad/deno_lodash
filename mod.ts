285678575eeed756a26aa4bfe1f971f8732cd1fa
Asad Ali <alidjasad@gmail.com>
2020-05-26 22:19:16 +0530

    setup complete

--- mod.ts
+++ mod.ts
@@ -1,8 +1,275 @@
-import { createRequire } from "https://deno.land/std/node/module.ts";
-const require = createRequire(import.meta.url);
-// Loads native module polyfill.
-const path = require("path");
+import add from "https://deno.land/x/lodash/add.js";
+import after from "https://deno.land/x/lodash/after.js";
+import at from "https://deno.land/x/lodash/at.js";
+import attempt from "https://deno.land/x/lodash/attempt.js";
+import before from "https://deno.land/x/lodash/before.js";
+import camelCase from "https://deno.land/x/lodash/camelCase.js";
+import capitalize from "https://deno.land/x/lodash/capitalize.js";
+import castArray from "https://deno.land/x/lodash/castArray.js";
+import ceil from "https://deno.land/x/lodash/ceil.js";
+import chunk from "https://deno.land/x/lodash/chunk.js";
+import clamp from "https://deno.land/x/lodash/clamp.js";
+import clone from "https://deno.land/x/lodash/clone.js";
+import cloneDeep from "https://deno.land/x/lodash/cloneDeep.js";
+import cloneDeepWith from "https://deno.land/x/lodash/cloneDeepWith.js";
+import cloneWith from "https://deno.land/x/lodash/cloneWith.js";
+import compact from "https://deno.land/x/lodash/compact.js";
+import cond from "https://deno.land/x/lodash/cond.js";
+import conforms from "https://deno.land/x/lodash/conforms.js";
+import conformsTo from "https://deno.land/x/lodash/conformsTo.js";
+import countBy from "https://deno.land/x/lodash/countBy.js";
+import create from "https://deno.land/x/lodash/create.js";
+import debounce from "https://deno.land/x/lodash/debounce.js";
+import deburr from "https://deno.land/x/lodash/deburr.js";
+import defaultTo from "https://deno.land/x/lodash/defaultTo.js";
+import defaultToAny from "https://deno.land/x/lodash/defaultToAny.js";
+import defaults from "https://deno.land/x/lodash/defaults.js";
+import defaultsDeep from "https://deno.land/x/lodash/defaultsDeep.js";
+import defer from "https://deno.land/x/lodash/defer.js";
+import delay from "https://deno.land/x/lodash/delay.js";
+import difference from "https://deno.land/x/lodash/difference.js";
+import differenceBy from "https://deno.land/x/lodash/differenceBy.js";
+import differenceWith from "https://deno.land/x/lodash/differenceWith.js";
+import divide from "https://deno.land/x/lodash/divide.js";
+import drop from "https://deno.land/x/lodash/drop.js";
+import dropRight from "https://deno.land/x/lodash/dropRight.js";
+import dropRightWhile from "https://deno.land/x/lodash/dropRightWhile.js";
+import dropWhile from "https://deno.land/x/lodash/dropWhile.js";
+import each from "https://deno.land/x/lodash/each.js";
+import eachRight from "https://deno.land/x/lodash/eachRight.js";
+import endsWith from "https://deno.land/x/lodash/endsWith.js";
+import eq from "https://deno.land/x/lodash/eq.js";
+import eqDeep from "https://deno.land/x/lodash/eqDeep.js";
+import escape from "https://deno.land/x/lodash/escape.js";
+import escapeRegExp from "https://deno.land/x/lodash/escapeRegExp.js";
+import every from "https://deno.land/x/lodash/every.js";
+import everyValue from "https://deno.land/x/lodash/everyValue.js";
+import filter from "https://deno.land/x/lodash/filter.js";
+import filterObject from "https://deno.land/x/lodash/filterObject.js";
+import findKey from "https://deno.land/x/lodash/findKey.js";
+import findLast from "https://deno.land/x/lodash/findLast.js";
+import findLastIndex from "https://deno.land/x/lodash/findLastIndex.js";
+import findLastKey from "https://deno.land/x/lodash/findLastKey.js";
+import first from "https://deno.land/x/lodash/first.js";
+import flatMap from "https://deno.land/x/lodash/flatMap.js";
+import flatMapDeep from "https://deno.land/x/lodash/flatMapDeep.js";
+import flatMapDepth from "https://deno.land/x/lodash/flatMapDepth.js";
+import flatten from "https://deno.land/x/lodash/flatten.js";
+import flattenDeep from "https://deno.land/x/lodash/flattenDeep.js";
+import flattenDepth from "https://deno.land/x/lodash/flattenDepth.js";
+import flip from "https://deno.land/x/lodash/flip.js";
+import floor from "https://deno.land/x/lodash/floor.js";
+import flow from "https://deno.land/x/lodash/flow.js";
+import flowRight from "https://deno.land/x/lodash/flowRight.js";
+import forEach from "https://deno.land/x/lodash/forEach.js";
+import forEachRight from "https://deno.land/x/lodash/forEachRight.js";
+import forOwn from "https://deno.land/x/lodash/forOwn.js";
+import forOwnRight from "https://deno.land/x/lodash/forOwnRight.js";
+import fromEntries from "https://deno.land/x/lodash/fromEntries.js";
+import functions from "https://deno.land/x/lodash/functions.js";
+import get from "https://deno.land/x/lodash/get.js";
+import groupBy from "https://deno.land/x/lodash/groupBy.js";
+import gt from "https://deno.land/x/lodash/gt.js";
+import gte from "https://deno.land/x/lodash/gte.js";
+import has from "https://deno.land/x/lodash/has.js";
+import hasIn from "https://deno.land/x/lodash/hasIn.js";
+import hasPath from "https://deno.land/x/lodash/hasPath.js";
+import hasPathIn from "https://deno.land/x/lodash/hasPathIn.js";
+import head from "https://deno.land/x/lodash/head.js";
+import inRange from "https://deno.land/x/lodash/inRange.js";
+import indexOf from "https://deno.land/x/lodash/indexOf.js";
+import initial from "https://deno.land/x/lodash/initial.js";
+import intersection from "https://deno.land/x/lodash/intersection.js";
+import intersectionBy from "https://deno.land/x/lodash/intersectionBy.js";
+import intersectionWith from "https://deno.land/x/lodash/intersectionWith.js";
+import invert from "https://deno.land/x/lodash/invert.js";
+import invertBy from "https://deno.land/x/lodash/invertBy.js";
+import invoke from "https://deno.land/x/lodash/invoke.js";
+import invokeMap from "https://deno.land/x/lodash/invokeMap.js";
+import isArguments from "https://deno.land/x/lodash/isArguments.js";
+import isArrayBuffer from "https://deno.land/x/lodash/isArrayBuffer.js";
+import isArrayLike from "https://deno.land/x/lodash/isArrayLike.js";
+import isArrayLikeObject from "https://deno.land/x/lodash/isArrayLikeObject.js";
+import isBoolean from "https://deno.land/x/lodash/isBoolean.js";
+import isBuffer from "https://deno.land/x/lodash/isBuffer.js";
+import isDate from "https://deno.land/x/lodash/isDate.js";
+import isElement from "https://deno.land/x/lodash/isElement.js";
+import isEmpty from "https://deno.land/x/lodash/isEmpty.js";
+import isEqualWith from "https://deno.land/x/lodash/isEqualWith.js";
+import isError from "https://deno.land/x/lodash/isError.js";
+import isFunction from "https://deno.land/x/lodash/isFunction.js";
+import isLength from "https://deno.land/x/lodash/isLength.js";
+import isMap  from "https://deno.land/x/lodash/isMap.js";
+import isMatch from "https://deno.land/x/lodash/isMatch.js";
+import isMatchWith from "https://deno.land/x/lodash/isMatchWith.js";
+import isNative from "https://deno.land/x/lodash/isNative.js";
+import isNil from "https://deno.land/x/lodash/isNil.js";
+import isNull from "https://deno.land/x/lodash/isNull.js";
+import isNumber from "https://deno.land/x/lodash/isNumber.js";
+import isObject from "https://deno.land/x/lodash/isObject.js";
+import isObjectLike from "https://deno.land/x/lodash/isObjectLike.js";
+import isPlainObject from "https://deno.land/x/lodash/isPlainObject.js";
+import isRegExp from "https://deno.land/x/lodash/isRegExp.js";
+import isSet from "https://deno.land/x/lodash/isSet.js";
+import isString from "https://deno.land/x/lodash/isString.js";
+import isSymbol from "https://deno.land/x/lodash/isSymbol.js";
+import isTypedArray from "https://deno.land/x/lodash/isTypedArray.js";
+import isUndefined from "https://deno.land/x/lodash/isUndefined.js";
+import isWeakMap from "https://deno.land/x/lodash/isWeakMap.js";
+import isWeakSet from "https://deno.land/x/lodash/isWeakSet.js";
+import kebabCase from "https://deno.land/x/lodash/kebabCase.js";
+import keyBy from "https://deno.land/x/lodash/keyBy.js";
+import keys from "https://deno.land/x/lodash/keys.js";
+import keysIn from "https://deno.land/x/lodash/keysIn.js";
+import last from "https://deno.land/x/lodash/last.js";
+import lastIndexOf from "https://deno.land/x/lodash/lastIndexOf.js";
+import lowerCase from "https://deno.land/x/lodash/lowerCase.js";
+import lowerFirst from "https://deno.land/x/lodash/lowerFirst.js";
+import lt from "https://deno.land/x/lodash/lt.js";
+import lte from "https://deno.land/x/lodash/lte.js";
+import map from "https://deno.land/x/lodash/map.js";
+import mapKey from "https://deno.land/x/lodash/mapKey.js";
+import mapObject from "https://deno.land/x/lodash/mapObject.js";
+import mapValue from "https://deno.land/x/lodash/mapValue.js";
+import matches from "https://deno.land/x/lodash/matches.js";
+import matchesProperty from "https://deno.land/x/lodash/matchesProperty.js";
+import maxBy from "https://deno.land/x/lodash/maxBy.js";
+import mean from "https://deno.land/x/lodash/mean.js";
+import meanBy from "https://deno.land/x/lodash/meanBy.js";
+import memoize from "https://deno.land/x/lodash/memoize.js";
+import merge from "https://deno.land/x/lodash/merge.js";
+import mergeWith from "https://deno.land/x/lodash/mergeWith.js";
+import method from "https://deno.land/x/lodash/method.js";
+import methodOf from "https://deno.land/x/lodash/methodOf.js";
+import minBy from "https://deno.land/x/lodash/minBy.js";
+import multiply from "https://deno.land/x/lodash/multiply.js";
+import negate from "https://deno.land/x/lodash/negate.js";
+import nth from "https://deno.land/x/lodash/nth.js";
+import nthArg from "https://deno.land/x/lodash/nthArg.js";
+import once from "https://deno.land/x/lodash/once.js";
+import orderBy from "https://deno.land/x/lodash/orderBy.js";
+import over from "https://deno.land/x/lodash/over.js";
+import overArgs from "https://deno.land/x/lodash/overArgs.js";
+import overEvery from "https://deno.land/x/lodash/overEvery.js";
+import overSome from "https://deno.land/x/lodash/overSome.js";
+import pad from "https://deno.land/x/lodash/pad.js";
+import padEnd from "https://deno.land/x/lodash/padEnd.js";
+import padStart from "https://deno.land/x/lodash/padStart.js";
+import parseInt from "https://deno.land/x/lodash/parseInt.js";
+import partition from "https://deno.land/x/lodash/partition.js";
+import pick from "https://deno.land/x/lodash/pick.js";
+import pickBy from "https://deno.land/x/lodash/pickBy.js";
+import property from "https://deno.land/x/lodash/property.js";
+import propertyOf from "https://deno.land/x/lodash/propertyOf.js";
+import pull from "https://deno.land/x/lodash/pull.js";
+import pullAll from "https://deno.land/x/lodash/pullAll.js";
+import pullAllBy from "https://deno.land/x/lodash/pullAllBy.js";
+import pullAllWith from "https://deno.land/x/lodash/pullAllWith.js";
+import pullAt from "https://deno.land/x/lodash/pullAt.js";
+import random from "https://deno.land/x/lodash/random.js";
+import range from "https://deno.land/x/lodash/range.js";
+import rangeRight from "https://deno.land/x/lodash/rangeRight.js";
+import reduce from "https://deno.land/x/lodash/reduce.js";
+import reduceRight from "https://deno.land/x/lodash/reduceRight.js";
+import reject from "https://deno.land/x/lodash/reject.js";
+import remove from "https://deno.land/x/lodash/remove.js";
+import repeat from "https://deno.land/x/lodash/repeat.js";
+import replace from "https://deno.land/x/lodash/replace.js";
+import result from "https://deno.land/x/lodash/result.js";
+import round from "https://deno.land/x/lodash/round.js";
+import sample from "https://deno.land/x/lodash/sample.js";
+import sampleSize from "https://deno.land/x/lodash/sampleSize.js";
+import set from "https://deno.land/x/lodash/set.js";
+import setWith from "https://deno.land/x/lodash/setWith.js";
+import shuffle from "https://deno.land/x/lodash/shuffle.js";
+import size from "https://deno.land/x/lodash/size.js";
+import slice from "https://deno.land/x/lodash/slice.js";
+import snakeCase from "https://deno.land/x/lodash/snakeCase.js";
+import some from "https://deno.land/x/lodash/some.js";
+import someValue from "https://deno.land/x/lodash/someValue.js";
+import sortedIndex from "https://deno.land/x/lodash/sortedIndex.js";
+import sortedIndexBy from "https://deno.land/x/lodash/sortedIndexBy.js";
+import sortedIndexOf from "https://deno.land/x/lodash/sortedIndexOf.js";
+import sortedLastIndex from "https://deno.land/x/lodash/sortedLastIndex.js";
+import sortedLastIndexBy from "https://deno.land/x/lodash/sortedLastIndexBy.js";
+import sortedLastIndexOf from "https://deno.land/x/lodash/sortedLastIndexOf.js";
+import sortedUniq from "https://deno.land/x/lodash/sortedUniq.js";
+import sortedUniqBy from "https://deno.land/x/lodash/sortedUniqBy.js";
+import split from "https://deno.land/x/lodash/split.js";
+import startCase from "https://deno.land/x/lodash/startCase.js";
+import startsWith from "https://deno.land/x/lodash/startsWith.js";
+import subtract from "https://deno.land/x/lodash/subtract.js";
+import sum from "https://deno.land/x/lodash/sum.js";
+import sumBy from "https://deno.land/x/lodash/sumBy.js";
+import tail from "https://deno.land/x/lodash/tail.js";
+import take from "https://deno.land/x/lodash/take.js";
+import takeRight from "https://deno.land/x/lodash/takeRight.js";
+import takeRightWhile from "https://deno.land/x/lodash/takeRightWhile.js";
+import takeWhile from "https://deno.land/x/lodash/takeWhile.js";
+import throttle from "https://deno.land/x/lodash/throttle.js";
+import times from "https://deno.land/x/lodash/times.js";
+import toArray from "https://deno.land/x/lodash/toArray.js";
+import toFinite from "https://deno.land/x/lodash/toFinite.js";
+import toInteger from "https://deno.land/x/lodash/toInteger.js";
+import toLength from "https://deno.land/x/lodash/toLength.js";
+import toNumber from "https://deno.land/x/lodash/toNumber.js";
+import toPath from "https://deno.land/x/lodash/toPath.js";
+import toPlainObject from "https://deno.land/x/lodash/toPlainObject.js";
+import toSafeInteger from "https://deno.land/x/lodash/toSafeInteger.js";
+import toString from "https://deno.land/x/lodash/toString.js";
+import transform from "https://deno.land/x/lodash/transform.js";
+import trim from "https://deno.land/x/lodash/trim.js";
+import trimEnd from "https://deno.land/x/lodash/trimEnd.js";
+import trimStart from "https://deno.land/x/lodash/trimStart.js";
+import truncate from "https://deno.land/x/lodash/truncate.js";
+import unescape from "https://deno.land/x/lodash/unescape.js";
+import union from "https://deno.land/x/lodash/union.js";
+import unionBy from "https://deno.land/x/lodash/unionBy.js";
+import unionWith from "https://deno.land/x/lodash/unionWith.js";
+import uniq from "https://deno.land/x/lodash/uniq.js";
+import uniqBy from "https://deno.land/x/lodash/uniqBy.js";
+import uniqWith from "https://deno.land/x/lodash/uniqWith.js";
+import uniqueId from "https://deno.land/x/lodash/uniqueId.js";
+import unset from "https://deno.land/x/lodash/unset.js";
+import unzip from "https://deno.land/x/lodash/unzip.js";
+import unzipWith from "https://deno.land/x/lodash/unzipWith.js";
+import update from "https://deno.land/x/lodash/update.js";
+import updateWith from "https://deno.land/x/lodash/updateWith.js";
+import upperCase from "https://deno.land/x/lodash/upperCase.js";
+import upperFirst from "https://deno.land/x/lodash/upperFirst.js";
+import values from "https://deno.land/x/lodash/values.js";
+import without from "https://deno.land/x/lodash/without.js";
+import words from "https://deno.land/x/lodash/words.js";
+import xor from "https://deno.land/x/lodash/xor.js";
+import xorBy from "https://deno.land/x/lodash/xorBy.js";
+import xorWith from "https://deno.land/x/lodash/xorWith.js";
+import zip from "https://deno.land/x/lodash/zip.js";
+import zipObject from "https://deno.land/x/lodash/zipObject.js";
+import zipObjectDeep from "https://deno.land/x/lodash/zipObjectDeep.js";
+import zipWith from "https://deno.land/x/lodash/zipWith.js";
 
-const _ = require('./index.js');
-
-export default _;
+export default {add, after, at, attempt, before, camelCase, capitalize, castArray, ceil,
+chunk, clamp, clone, cloneDeep, cloneDeepWith, compact, cloneWith, cond, conforms, conformsTo, countBy,
+create, debounce, deburr, defaults, defaultTo, defaultToAny, defaultsDeep,defer, delay, difference,
+differenceBy, differenceWith, divide, drop, dropRight, dropRightWhile, dropWhile, each, eachRight,
+endsWith, eq, eqDeep, escape, escapeRegExp, every, everyValue, filter, filterObject, findKey,
+findLast, findLastIndex, findLastKey, first, flatMap, flatMapDeep, flatMapDepth, flatten,
+flattenDeep, flattenDepth, flip, floor, flow, flowRight, forEach, forEachRight, forOwn, forOwnRight,
+fromEntries, functions, get, groupBy, gt, gte, has, hasIn, hasPath, hasPathIn, head, inRange,
+indexOf, initial, intersection, intersectionBy, intersectionWith, invert, invertBy, invoke,
+invokeMap, isArguments, isArrayBuffer, isArrayLike, isArrayLikeObject, isBoolean, isBuffer, isDate,
+isElement, isEmpty, isEqualWith, isError, isFunction, isLength, isMap, isMatch, isMatchWith,
+isNative, isNil, isNull, isNumber, isObject, isObjectLike, isPlainObject, isRegExp, isSet,
+isString, isSymbol, isTypedArray, isUndefined, isWeakMap, isWeakSet, kebabCase, keyBy, keys,
+keysIn, last, lastIndexOf, lowerCase, lowerFirst, lt, lte, map, mapKey, mapObject, mapValue,
+matches, matchesProperty, maxBy, mean, meanBy, memoize, merge, mergeWith, method, methodOf,
+minBy, multiply, negate, nth, nthArg, once, orderBy, over, overArgs, overEvery, overSome, pad,
+padEnd, padStart, parseInt, partition, pick, pickBy, property, propertyOf, pull, pullAll, pullAllBy,
+pullAllWith, pullAt, random, range, rangeRight, reduce, reduceRight, reject, remove, repeat,
+replace, result, round, sample, sampleSize, set, setWith, shuffle, size, slice, snakeCase,
+some, someValue, sortedIndex, sortedIndexBy, sortedIndexOf, sortedLastIndex, sortedLastIndexBy, sortedLastIndexOf,
+sortedUniq, sortedUniqBy, split, startCase, startsWith, subtract, sum, sumBy, tail, take, takeRight,
+takeRightWhile, takeWhile, throttle, times, toArray, toFinite, toInteger, toLength, toNumber, toPath, toPlainObject,
+toSafeInteger, toString, transform, trim, trimEnd, trimStart,truncate, unescape, union, unionBy, unionWith, uniq, uniqBy, uniqWith, uniqueId, unset,
+unzip, unzipWith, update, updateWith, upperCase, upperFirst, values, without, words, xor, xorBy,
+xorWith, zip, zipObject, zipObjectDeep, zipWith};
